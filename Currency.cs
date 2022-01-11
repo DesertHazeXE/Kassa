962d03c4bc7d48a8760f00442f6338ae0154fca3
DesertHazeXE <davidtg@code.edu.az>
2022-01-11 11:54:45 +0400

    first commit

---
+++ Currency.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kassa.Enums
+{
+    enum Currency
+    {
+        Azn,
+        Dollar
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Kassa.Models;
+using System;
+
+namespace Kassa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CashRegister cashRegister = new CashRegister(0,Enums.Currency.Azn);
+            cashRegister.AddNewSales(10, Enums.Currency.Azn);
+            cashRegister.AddNewSales(10, Enums.Currency.Dollar);
+            cashRegister.RemoveSales(10, Enums.Currency.Dollar);
+            Console.WriteLine(cashRegister);
+        }
+    }
+}
